fa1fe53bde12bb5c31ffe0c32cb4877b637989da
b5019525 <b5019525@my.shu.ac.uk>
2017-01-19 13:56:31 +0000

    created Module domain class

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,8 +1,21 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
     def init = { servletContext ->
+		def computing = new Course(
+		title:'BSc Hon Computing',
+		code:'COMP234',
+		leader:'Dr Mary Poppins',
+		department:'Computing',
+		startDate:new Date('11/11/2016'),
+		endDate:new Date('11/11/2020'),
+		numberOfStudents:45,
+		description:''' Add some text here''',
+
+		tuitionFees:9000,
+		studyMode:'Full-time'
+	).save()
     }
     def destroy = {
     }
