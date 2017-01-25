9fc44f57df4ce8724a9d1dff0e3e3be62810d60c
b5019525 <b5019525@my.shu.ac.uk>
2017-01-25 15:58:07 +0000

    Created domain class lecturer

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -16,6 +16,39 @@ class BootStrap {
 		tuitionFees:9000,
 		studyMode:'Full-time'
 	).save()
+    
+	
+		def pcp = new Module(
+		title:'PCP',
+		code:'COMP234',
+		credits:20,
+		lecturer:'Dr Mary Poppins',
+		course:'BSc Hon Computing',
+		description:''' Add some text here''',
+
+	).save()
+
+		def student1 = new Student(
+		name: 'Scott Talbot',		
+		studentID: 'B5019525',
+		dob: new Date('11/11/1997'),
+		isFundingAvalable: 'true',
+		email: 'b5019525@my.shu.ac.uk',
+		username: 'b5019525',
+		password: 'password',
+		course: 'Computing',
+	
+	).save()
+
+		def lecturer1 = new Lecturer(
+		fullName: 'Ryan Johnson',		
+		post: 'Leader',
+		subject: 'Web Development',
+		lecturerEmail: 'b5119525@my.shu.ac.uk',
+		office: 'O322',
+		bio:''' Add some text here''',
+	
+	).save()
     }
     def destroy = {
     }
